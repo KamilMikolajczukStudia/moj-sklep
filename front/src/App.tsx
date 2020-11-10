de3e0a088cacb40c36a5d86363a7f48c2cbae0f3
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2020-11-10 22:23:11 +0100

    logowanie i rejestracja

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+
 import SignIn from './SignIn'
+import Register from './SignIn/Register'
+
+enum AppStates { user, signIn, register }
+
+function ApplicationState() {
+  const [state, setState] = useState(AppStates.signIn)
+
+  const loginUser = useCallback((token: string) => {
+    setState(AppStates.user)
+  }, [])
+
+  const signIn = useCallback(() => {
+    setState(AppStates.signIn)
+  }, [])
+
+  const register = useCallback(() => {
+    setState(AppStates.register)
+  }, [])
+
+  switch(state) {
+    case AppStates.signIn:
+      return <SignIn register={register} />
+    case AppStates.register:
+      return <Register signIn={signIn} />
+    default:
+      return null
+  }
+}
 
 function App() {
   return (
-    <SignIn />
+    <ApplicationState></ApplicationState>
+    
   )
 }
 
